10a1f178ae8cf18cf8cf0bf1543065c9fd722de8
Kemuel M. López <kemuel1024@hotmail.com>
2021-10-13 23:45:22 -0600

    Commit #1
    Creación del Esqueleto del modulo Open Academy

---
+++ open_academy/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ../industria/openAcademy(http.Controller):
+#     @http.route('/../industria/open_academy/../industria/open_academy/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/../industria/open_academy/../industria/open_academy/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('../industria/open_academy.listing', {
+#             'root': '/../industria/open_academy/../industria/open_academy',
+#             'objects': http.request.env['../industria/open_academy.../industria/open_academy'].search([]),
+#         })
+
+#     @http.route('/../industria/open_academy/../industria/open_academy/objects/<model("../industria/open_academy.../industria/open_academy"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('../industria/open_academy.object', {
+#             'object': obj
+#         })
